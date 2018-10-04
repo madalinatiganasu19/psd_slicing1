8609e47479f5f190d91f7a77d1b03557f6ed7de0
Madalina Tiganasu <madalina.tiganasu@5psolutions.eu>
2018-10-04 14:30:20 +0300

    #25: PSD slicing http://pm.5psolutions.net/public/index.php?path_info=projects/88/tasks/25 make slick slider responsive

--- js/init-slick.js
+++ js/init-slick.js
@@ -5,6 +5,25 @@ window.onload=function(){
         arrows:false,
         centerMode:true,
         slidesToShow:3,
-        slidesToScroll:1
+        slidesToScroll:1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 };
